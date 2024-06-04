de8daddb52e6eec64cd492dff9148771159ed7d0
laamnguyenn <nguyenphuclam16@gmail.com>
2024-06-05 00:00:05 +0700

    Lam pull Product

---
+++ product.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Record Store</title>
+    <link rel="icon" type="image/x-icon" href="./images/header/logo.png">
+</head>
+<body>
+    <?php include './includes/header.php'?>
+    
+    <p>Hoang Hieu demo</p>
+
+    <?php include './includes/footer.php'?>
+</body>
+</html>
