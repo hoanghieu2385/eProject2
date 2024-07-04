4c7c36ff7a57d8f350a5b009e7a5fdfa636fe377
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-07-04 23:53:28 +0700

    sửa tên web + add logo web

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Tortured Poets Department</title>
+    <title>Hoot Records</title>
+    <link rel="icon" type="image/x-icon" href="./images/header/logo.png">
     <link rel="stylesheet" href="./css/index.css">
     <script defer src="./js/index.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
