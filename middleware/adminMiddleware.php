81a82be983e0043f5780ae0f20105511cd770536
LHHDANG <142959277+LHHDANG@users.noreply.github.com>
2024-07-11 23:54:11 +0700

    day code

--- middleware/adminMiddleware.php
+++ middleware/adminMiddleware.php
@@ -1,14 +1,11 @@
 <?php
-
 include('../functions/myfunctions.php');
 
 if (isset($_SESSION['auth'])) {
-
-    if ($_SESSION['role_as'] != 1) {
-
+    if (isset($_SESSION['auth_user']['role_as']) && $_SESSION['auth_user']['role_as'] != 1) {
         redirect("../index.php", "You are not authorized to access this page.");
     }
 } else {
-
     redirect("../login/login.php", "Please login to continue.");
 }
+?>
