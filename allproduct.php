ce2bd3996437cd9ee69783f086678a8d70c1a4d6
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-07-22 19:14:00 +0700

    sửa phần categories trên menu header,

--- allproduct.php
+++ allproduct.php
@@ -115,6 +115,8 @@ $conn->close();
     <title>Buy Records from our Online Store</title>
     <link rel="icon" type="image/x-icon" href="./images/header/logo.png">
     <link rel="stylesheet" href="./css/allproduct.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
