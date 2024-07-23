e1618ca6c72ec609b5f82e6c6b83b90c4f45c57d
lttungog <124686851+lttungog@users.noreply.github.com>
2024-07-23 19:43:28 +0700

    Tung dang chinh order.php va pagination cho order.php. Tung update contact_us - chinh google api va text banner.

--- admin/order.php
+++ admin/order.php
@@ -7,6 +7,28 @@ include('../middleware/adminMiddleware.php');
 // // the header.php include was previously on top of adminMiddleware include
 // include('includes/header.php');
 
+// Set the number of orders per page
+$orders_per_page = 7;
+
+// Get the current page number from the URL, default to 1 if not set
+if (isset($_GET['page']) && is_numeric($_GET['page'])) {
+    $current_page = (int) $_GET['page'];
+} else {
+    $current_page = 1;
+}
+
+// Calculate the offset for the SQL query
+$offset = ($current_page - 1) * $orders_per_page;
+
+// Get the total number of orders
+$total_orders_query = "SELECT COUNT(*) AS total FROM order";
+$total_orders_result = mysqli_query($con, $total_orders_query);
+$total_orders_row = mysqli_fetch_assoc($total_orders_result);
+$total_orders = $total_orders_row['total'];
+
+// Calculate the total number of pages
+$total_pages = ceil($total_orders / $orders_per_page);
+
 ?>
 
 <div class="py-5">
@@ -32,11 +54,47 @@ include('../middleware/adminMiddleware.php');
                                 $orders = getAllOrders(); 
                                 // We have created this function in myfunctions.php. Check if there is a need to anything else. Moving back to payment_shipment because it's necessary for shop_order.
                             
+                                $query = "
+                                SELECT p.id, pc.category_name, a.full_name as artist_name, p.album, p.version, p.edition, p.product_image, p.current_price 
+                                FROM product p
+                                JOIN product_category pc ON p.category_id = pc.id
+                                JOIN artist a ON p.artist_id = a.id
+                                ORDER BY p.id DESC
+                                LIMIT $offset, $orders_per_page
+                                ";
+                                // keep line 63 (LIMIT) - it's used for pagination, change the other lines of query to fit use
+
                             ?>
 
                         </tbody>
                     </table>
                 </div>
+                <!-- Pagination controls -->
+                <nav>
+                        <ul class="pagination justify-content-center" style="margin-top:15px">
+                            <?php if ($current_page > 1): ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="?page=<?= $current_page - 1 ?>" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                            <?php endif; ?>
+
+                            <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                                <li class="page-item <?= $i == $current_page ? 'active ' : '' ?>">
+                                    <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                                </li>
+                            <?php endfor; ?>
+
+                            <?php if ($current_page < $total_pages): ?>
+                                <li class="page-item">
+                                    <a class="page-link" href="?page=<?= $current_page + 1 ?>" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            <?php endif; ?>
+                        </ul>
+                    </nav>
             </div>
         </div>
     </div>
--- contact_us.php
+++ contact_us.php
@@ -12,7 +12,7 @@
     <div class="contact-section">
         <div class="contact-header">
             <h1>CONTACT US</h1>
-            <p>Need an expert? You are more than welcomed to leave your contact info and we will be in touch softly</p>
+            <p>Need an expert? You are more than welcomed to leave your contact info and we will be in touch shortly</p>
         </div>
         <div class="contact-info">
             <div class="contact-item">
@@ -32,7 +32,7 @@
             </div>
         </div>
         <div class="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.026421835407!2d105.82508671476372!3d21.030152792946424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab9d82a2c6e7%3A0x68baf5bb9497e1d4!2zU3BsZW5kb3Jh!5e0!3m2!1sen!2s!4v1625794055293!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.4165201753567!2d105.72026177556093!3d21.016013688215676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313453634fe7354f%3A0x527efce91b98bb13!2sSplendora%20CH%20103!5e0!3m2!1sen!2s!4v1721737310791!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
     <?php include './includes/footer.php'?>
